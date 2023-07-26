88675b8e975d7284b938f4bfd6b1d11600e26a0f
sinjikoro <shinnaga.dev@gmail.com>
2023-07-26 22:27:52 +0900

    a

--- functions/mockoon.js
+++
@@ -1,8 +0,0 @@
-const mockoon = require('@mockoon/serverless');
-
-// Load the Mockoon Environment object
-const mockEnv = require('./demo.json');
-
-const mockoonServerless = new mockoon.MockoonServerless(mockEnv);
-
-exports.handler = mockoonServerless.netlifyHandler();

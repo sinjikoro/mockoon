0ea4ababf6503275092bb1ac971c3f33f4e84286
sinjikoro <shinnaga.dev@gmail.com>
2023-07-27 00:30:02 +0900

    a

--- netlify/functions/mockoon/mockoon.js
+++ netlify/functions/mockoon/mockoon.js
@@ -1,5 +1,5 @@
 const mockoon = require('@mockoon/serverless')
-const mockEnv = require('../../../api/api.json')
+const mockEnv = require('../../../api/demo.json')
 const mockoonServerless = new mockoon.MockoonServerless(mockEnv)
 
 exports.handler = mockoonServerless.netlifyHandler()
